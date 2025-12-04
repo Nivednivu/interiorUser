823fc6895c71de3aad99505754b5b87f8005e344
Nivednivu <142215596+Nivednivu@users.noreply.github.com>
2025-12-04 12:52:40 +0530

    Update API endpoint for fetching products

--- src/pages/ProductSlider/ProductSlider.jsx
+++ src/pages/ProductSlider/ProductSlider.jsx
@@ -18,7 +18,7 @@ const ProductSlider = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await axios.get('https://interiorserverfinal.onrender.com/api/products');
+      const response = await axios.get('https://interiorservermongo.onrender.com/api/products');
       setProducts(response.data.data);
     } catch (error) {
       console.error('Error fetching products:', error);
